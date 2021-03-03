f1c6610d5d46c11f1b56becca0231ddcf9e39131
Sirin Chankao <sirin.cha@student.mahidol.edu>
2021-03-03 13:42:07 +0700

    Setup Chakra UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
 import logo from './assets/logo.svg';
 import './assets/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ChakraProvider>
   );
 }
 
