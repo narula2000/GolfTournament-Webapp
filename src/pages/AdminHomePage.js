f10a2d2baf49cdd8844e7fe036e80dc96224490e
Phairat Lin <yangsheng104@gmail.com>
2021-03-13 18:50:20 +0700

    Can collect all holes information. Can display the summary when clicking create button. Only task left is pushing  data into the database

--- src/pages/AdminHomePage.js
+++ src/pages/AdminHomePage.js
@@ -23,8 +23,132 @@ import logo from '../assets/golf-logo.png';
 const AdminHomePage = () => {
   const [name, setName] = useState('');
   const [isOpen, setIsOpen] = useState(false);
+  const [outPar, setOut] = useState(0);
+  const [inPar, setIn] = useState(0);
+  const [outSi, setOutSI] = useState(0);
+  const [inSi, setInSI] = useState(0);
+  const [holes, setHoles] = useState({
+    hole1: {
+      par: 0,
+      strokeIndex: 0,
+    },
+    hole2: {
+      par: 0,
+      strokeIndex: 0,
+    },
+    hole3: {
+      par: 0,
+      strokeIndex: 0,
+    },
+    hole4: {
+      par: 0,
+      strokeIndex: 0,
+    },
+    hole5: {
+      par: 0,
+      strokeIndex: 0,
+    },
+    hole6: {
+      par: 0,
+      strokeIndex: 0,
+    },
+    hole7: {
+      par: 0,
+      strokeIndex: 0,
+    },
+    hole8: {
+      par: 0,
+      strokeIndex: 0,
+    },
+    hole9: {
+      par: 0,
+      strokeIndex: 0,
+    },
+    hole10: {
+      par: 0,
+      strokeIndex: 0,
+    },
+    hole11: {
+      par: 0,
+      strokeIndex: 0,
+    },
+    hole12: {
+      par: 0,
+      strokeIndex: 0,
+    },
+    hole13: {
+      par: 0,
+      strokeIndex: 0,
+    },
+    hole14: {
+      par: 0,
+      strokeIndex: 0,
+    },
+    hole15: {
+      par: 0,
+      strokeIndex: 0,
+    },
+    hole16: {
+      par: 0,
+      strokeIndex: 0,
+    },
+    hole17: {
+      par: 0,
+      strokeIndex: 0,
+    },
+    hole18: {
+      par: 0,
+      strokeIndex: 0,
+    },
+  });
   const onClose = () => setIsOpen(false);
   const cancelRef = React.useRef();
+
+  const handlePar = (event, holeData, holeName) => {
+    setHoles(() => {
+      const data = { ...holes };
+      data[holeName] = {
+        ...holeData,
+        par: Number.isNaN(parseInt(event.target.value, 10))
+          ? 0
+          : parseInt(event.target.value, 10),
+      };
+      return data;
+    });
+  };
+  const handleStrokeIndex = (event, holeData, holeName) => {
+    setHoles(() => {
+      const data = { ...holes };
+      data[holeName] = {
+        ...holeData,
+        strokeIndex: Number.isNaN(parseInt(event.target.value, 10))
+          ? 0
+          : parseInt(event.target.value, 10),
+      };
+      return data;
+    });
+  };
+  const totalCount = () => {
+    let outParCount = 0;
+    let outSiCount = 0;
+    let inParCount = 0;
+    let inSiCount = 0;
+    let flip = true;
+    Object.keys(holes).forEach((key) => {
+      if (key.slice(-1) === '0') flip = false;
+      if (flip) {
+        outParCount += holes[key].par;
+        outSiCount += holes[key].strokeIndex;
+      } else {
+        inParCount += holes[key].par;
+        inSiCount += holes[key].strokeIndex;
+      }
+    });
+    setOut(outParCount);
+    setOutSI(outSiCount);
+    setIn(inParCount);
+    setInSI(inSiCount);
+  };
 
   return (
     <Box background="white">
@@ -38,7 +162,7 @@ const AdminHomePage = () => {
       <Box
         position="absolute"
         background="#7FD661"
-        width="600px"
+        width="550px"
         height="600px"
         left="80px"
         top="200px"
@@ -56,7 +180,7 @@ const AdminHomePage = () => {
             width="380px"
             background="white"
             border="3px solid #BBBBBB"
-            onChange={(event) => setName(event.target.value)}
+            onChangeCapture={(event) => setName(event.target.value)}
           />
           <InputGroup width="480px">
             <Stack direction="row" spacing={4} align="center">
@@ -64,14 +188,20 @@ const AdminHomePage = () => {
                 <InputLeftAddon>Hole 1</InputLeftAddon>
                 <Input
                   placeholder="Par"
+                  type="text"
                   background="white"
                   border="3px solid #BBBBBB"
+                  onChange={(event) => handlePar(event, holes.hole1, 'hole1')}
                 />
               </InputGroup>
               <Input
                 placeholder="Stroke Index"
+                type="text"
                 background="white"
                 border="3px solid #BBBBBB"
+                onChange={(event) =>
+                  handleStrokeIndex(event, holes.hole1, 'hole1')
+                }
               />
             </Stack>
           </InputGroup>
@@ -81,14 +211,20 @@ const AdminHomePage = () => {
                 <InputLeftAddon>Hole 2</InputLeftAddon>
                 <Input
                   placeholder="Par"
+                  type="text"
                   background="white"
                   border="3px solid #BBBBBB"
+                  onChange={(event) => handlePar(event, holes.hole1, 'hole2')}
                 />
               </InputGroup>
               <Input
                 placeholder="Stroke Index"
+                type="text"
                 background="white"
                 border="3px solid #BBBBBB"
+                onChange={(event) =>
+                  handleStrokeIndex(event, holes.hole1, 'hole2')
+                }
               />
             </Stack>
           </InputGroup>
@@ -98,14 +234,20 @@ const AdminHomePage = () => {
                 <InputLeftAddon>Hole 3</InputLeftAddon>
                 <Input
                   placeholder="Par"
+                  type="text"
                   background="white"
                   border="3px solid #BBBBBB"
+                  onChange={(event) => handlePar(event, holes.hole1, 'hole3')}
                 />
               </InputGroup>
               <Input
                 placeholder="Stroke Index"
+                type="text"
                 background="white"
                 border="3px solid #BBBBBB"
+                onChange={(event) =>
+                  handleStrokeIndex(event, holes.hole1, 'hole3')
+                }
               />
             </Stack>
           </InputGroup>
@@ -115,14 +257,20 @@ const AdminHomePage = () => {
                 <InputLeftAddon>Hole 4</InputLeftAddon>
                 <Input
                   placeholder="Par"
+                  type="text"
                   background="white"
                   border="3px solid #BBBBBB"
+                  onChange={(event) => handlePar(event, holes.hole1, 'hole4')}
                 />
               </InputGroup>
               <Input
                 placeholder="Stroke Index"
+                type="text"
                 background="white"
                 border="3px solid #BBBBBB"
+                onChange={(event) =>
+                  handleStrokeIndex(event, holes.hole1, 'hole4')
+                }
               />
             </Stack>
           </InputGroup>
@@ -132,14 +280,20 @@ const AdminHomePage = () => {
                 <InputLeftAddon>Hole 5</InputLeftAddon>
                 <Input
                   placeholder="Par"
+                  type="text"
                   background="white"
                   border="3px solid #BBBBBB"
+                  onChange={(event) => handlePar(event, holes.hole1, 'hole5')}
                 />
               </InputGroup>
               <Input
                 placeholder="Stroke Index"
+                type="text"
                 background="white"
                 border="3px solid #BBBBBB"
+                onChange={(event) =>
+                  handleStrokeIndex(event, holes.hole1, 'hole5')
+                }
               />
             </Stack>
           </InputGroup>
@@ -149,14 +303,20 @@ const AdminHomePage = () => {
                 <InputLeftAddon>Hole 6</InputLeftAddon>
                 <Input
                   placeholder="Par"
+                  type="text"
                   background="white"
                   border="3px solid #BBBBBB"
+                  onChange={(event) => handlePar(event, holes.hole1, 'hole6')}
                 />
               </InputGroup>
               <Input
                 placeholder="Stroke Index"
+                type="text"
                 background="white"
                 border="3px solid #BBBBBB"
+                onChange={(event) =>
+                  handleStrokeIndex(event, holes.hole1, 'hole6')
+                }
               />
             </Stack>
           </InputGroup>
@@ -166,14 +326,20 @@ const AdminHomePage = () => {
                 <InputLeftAddon>Hole 7</InputLeftAddon>
                 <Input
                   placeholder="Par"
+                  type="text"
                   background="white"
                   border="3px solid #BBBBBB"
+                  onChange={(event) => handlePar(event, holes.hole1, 'hole7')}
                 />
               </InputGroup>
               <Input
                 placeholder="Stroke Index"
+                type="text"
                 background="white"
                 border="3px solid #BBBBBB"
+                onChange={(event) =>
+                  handleStrokeIndex(event, holes.hole1, 'hole7')
+                }
               />
             </Stack>
           </InputGroup>
@@ -183,14 +349,20 @@ const AdminHomePage = () => {
                 <InputLeftAddon>Hole 8</InputLeftAddon>
                 <Input
                   placeholder="Par"
+                  type="text"
                   background="white"
                   border="3px solid #BBBBBB"
+                  onChange={(event) => handlePar(event, holes.hole1, 'hole8')}
                 />
               </InputGroup>
               <Input
                 placeholder="Stroke Index"
+                type="text"
                 background="white"
                 border="3px solid #BBBBBB"
+                onChange={(event) =>
+                  handleStrokeIndex(event, holes.hole1, 'hole8')
+                }
               />
             </Stack>
           </InputGroup>
@@ -200,14 +372,20 @@ const AdminHomePage = () => {
                 <InputLeftAddon>Hole 9</InputLeftAddon>
                 <Input
                   placeholder="Par"
+                  type="text"
                   background="white"
                   border="3px solid #BBBBBB"
+                  onChange={(event) => handlePar(event, holes.hole1, 'hole9')}
                 />
               </InputGroup>
               <Input
                 placeholder="Stroke Index"
+                type="text"
                 background="white"
                 border="3px solid #BBBBBB"
+                onChange={(event) =>
+                  handleStrokeIndex(event, holes.hole1, 'hole9')
+                }
               />
             </Stack>
           </InputGroup>
@@ -216,7 +394,7 @@ const AdminHomePage = () => {
       <Box
         position="absolute"
         background="#7FD661"
-        width="600px"
+        width="550px"
         height="600px"
         right="80px"
         top="200px"
@@ -234,15 +412,21 @@ const AdminHomePage = () => {
               <InputGroup>
                 <InputLeftAddon>Hole 10</InputLeftAddon>
                 <Input
+                  type="text"
                   placeholder="Par"
                   background="white"
                   border="3px solid #BBBBBB"
+                  onChange={(event) => handlePar(event, holes.hole1, 'hole10')}
                 />
               </InputGroup>
               <Input
+                type="text"
                 placeholder="Stroke Index"
                 background="white"
                 border="3px solid #BBBBBB"
+                onChange={(event) =>
+                  handleStrokeIndex(event, holes.hole1, 'hole10')
+                }
               />
             </Stack>
           </InputGroup>
@@ -252,14 +436,20 @@ const AdminHomePage = () => {
                 <InputLeftAddon>Hole 11</InputLeftAddon>
                 <Input
                   placeholder="Par"
+                  type="text"
                   background="white"
                   border="3px solid #BBBBBB"
+                  onChange={(event) => handlePar(event, holes.hole1, 'hole11')}
                 />
               </InputGroup>
               <Input
                 placeholder="Stroke Index"
+                type="text"
                 background="white"
                 border="3px solid #BBBBBB"
+                onChange={(event) =>
+                  handleStrokeIndex(event, holes.hole1, 'hole11')
+                }
               />
             </Stack>
           </InputGroup>
@@ -269,14 +459,20 @@ const AdminHomePage = () => {
                 <InputLeftAddon>Hole 12</InputLeftAddon>
                 <Input
                   placeholder="Par"
+                  type="text"
                   background="white"
                   border="3px solid #BBBBBB"
+                  onChange={(event) => handlePar(event, holes.hole1, 'hole12')}
                 />
               </InputGroup>
               <Input
                 placeholder="Stroke Index"
+                type="text"
                 background="white"
                 border="3px solid #BBBBBB"
+                onChange={(event) =>
+                  handleStrokeIndex(event, holes.hole1, 'hole12')
+                }
               />
             </Stack>
           </InputGroup>
@@ -286,14 +482,20 @@ const AdminHomePage = () => {
                 <InputLeftAddon>Hole 13</InputLeftAddon>
                 <Input
                   placeholder="Par"
+                  type="text"
                   background="white"
                   border="3px solid #BBBBBB"
+                  onChange={(event) => handlePar(event, holes.hole1, 'hole13')}
                 />
               </InputGroup>
               <Input
                 placeholder="Stroke Index"
+                type="text"
                 background="white"
                 border="3px solid #BBBBBB"
+                onChange={(event) =>
+                  handleStrokeIndex(event, holes.hole1, 'hole13')
+                }
               />
             </Stack>
           </InputGroup>
@@ -303,14 +505,20 @@ const AdminHomePage = () => {
                 <InputLeftAddon>Hole 14</InputLeftAddon>
                 <Input
                   placeholder="Par"
+                  type="text"
                   background="white"
                   border="3px solid #BBBBBB"
+                  onChange={(event) => handlePar(event, holes.hole1, 'hole14')}
                 />
               </InputGroup>
               <Input
                 placeholder="Stroke Index"
+                type="text"
                 background="white"
                 border="3px solid #BBBBBB"
+                onChange={(event) =>
+                  handleStrokeIndex(event, holes.hole1, 'hole14')
+                }
               />
             </Stack>
           </InputGroup>
@@ -320,14 +528,20 @@ const AdminHomePage = () => {
                 <InputLeftAddon>Hole 15</InputLeftAddon>
                 <Input
                   placeholder="Par"
+                  type="text"
                   background="white"
                   border="3px solid #BBBBBB"
+                  onChange={(event) => handlePar(event, holes.hole1, 'hole15')}
                 />
               </InputGroup>
               <Input
                 placeholder="Stroke Index"
+                type="text"
                 background="white"
                 border="3px solid #BBBBBB"
+                onChange={(event) =>
+                  handleStrokeIndex(event, holes.hole1, 'hole15')
+                }
               />
             </Stack>
           </InputGroup>
@@ -337,14 +551,20 @@ const AdminHomePage = () => {
                 <InputLeftAddon>Hole 16</InputLeftAddon>
                 <Input
                   placeholder="Par"
+                  type="text"
                   background="white"
                   border="3px solid #BBBBBB"
+                  onChange={(event) => handlePar(event, holes.hole1, 'hole16')}
                 />
               </InputGroup>
               <Input
                 placeholder="Stroke Index"
+                type="text"
                 background="white"
                 border="3px solid #BBBBBB"
+                onChange={(event) =>
+                  handleStrokeIndex(event, holes.hole1, 'hole16')
+                }
               />
             </Stack>
           </InputGroup>
@@ -354,14 +574,20 @@ const AdminHomePage = () => {
                 <InputLeftAddon>Hole 17</InputLeftAddon>
                 <Input
                   placeholder="Par"
+                  type="text"
                   background="white"
                   border="3px solid #BBBBBB"
+                  onChange={(event) => handlePar(event, holes.hole1, 'hole17')}
                 />
               </InputGroup>
               <Input
                 placeholder="Stroke Index"
+                type="text"
                 background="white"
                 border="3px solid #BBBBBB"
+                onChange={(event) =>
+                  handleStrokeIndex(event, holes.hole1, 'hole17')
+                }
               />
             </Stack>
           </InputGroup>
@@ -371,14 +597,20 @@ const AdminHomePage = () => {
                 <InputLeftAddon>Hole 18</InputLeftAddon>
                 <Input
                   placeholder="Par"
+                  type="text"
                   background="white"
                   border="3px solid #BBBBBB"
+                  onChange={(event) => handlePar(event, holes.hole1, 'hole18')}
                 />
               </InputGroup>
               <Input
                 placeholder="Stroke Index"
+                type="text"
                 background="white"
                 border="3px solid #BBBBBB"
+                onChange={(event) =>
+                  handleStrokeIndex(event, holes.hole1, 'hole18')
+                }
               />
             </Stack>
           </InputGroup>
@@ -389,7 +621,10 @@ const AdminHomePage = () => {
           borderRadius="20px"
           color="white"
           top="20px"
-          onClick={() => setIsOpen(true)}
+          onClick={() => {
+            setIsOpen(true);
+            totalCount();
+          }}
         >
           Create Tournament
         </Button>
@@ -409,16 +644,16 @@ const AdminHomePage = () => {
           <AlertDialogBody>
             <Heading>{name}</Heading>
             <br />
-            <Text>Total Par of OUT is </Text>
-            <Text>Total Par of IN is</Text>
+            <Text>Total Out Par: {outPar}</Text>
+            <Text>Total Out Stroke Index: {outSi}</Text>
+            <Text>Total In Par: {inPar}</Text>
+            <Text>Total In Stroke Index: {inSi}</Text>
           </AlertDialogBody>
           <AlertDialogFooter>
-            <Button ref={cancelRef} onClick={onClose}>
+            <Button ref={cancelRef} colorScheme="red" onClick={onClose}>
               No
             </Button>
-            <Button colorScheme="red" ml={3}>
-              Yes
-            </Button>
+            <Button ml={3}>Yes</Button>
           </AlertDialogFooter>
         </AlertDialogContent>
       </AlertDialog>
