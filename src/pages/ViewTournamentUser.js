f84a1c7b2ba07f2d8f06e57b8de6d097a2109916
Sirin Chankao <sirin.cha@student.mahidol.edu>
2021-03-15 20:10:34 +0700

    Create View Tournament User page

---
+++ src/pages/ViewTournamentUser.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Container } from '@chakra-ui/react';
+
+const ViewTournamentUser = () => <Container>View User Tournament</Container>;
+
+export default ViewTournamentUser;
--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,12 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import AdminLoginPage from '../pages/AdminLoginPage';
 import RankingHomePage from '../pages/RankingHomePage';
 import AdminDashboard from '../pages/AdminDashboard';
+import ViewTournamentUser from '../pages/ViewTournamentUser';
 
 const router = () => (
   <Router>
     <Switch>
+      <Route path="/admin/tournamentuser" component={ViewTournamentUser} />
       <Route path="/admin/dashboard" component={AdminDashboard} />
       <Route path="/admin" component={AdminLoginPage} />
       <Route path="/" component={RankingHomePage} />
