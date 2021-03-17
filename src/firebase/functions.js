e9ccb6c4b2e8adcb7ae65761ba0be0557df22152
Vikrom Narula <vikrom.nar@gmail.com>
2021-03-17 22:13:04 +0700

    added addUser and deleteUser fixed createTournament to await setting data

--- src/firebase/functions.js
+++ src/firebase/functions.js
@@ -26,9 +26,34 @@ const createTournament = async (_adminId, _tournamentId, holesData) => {
     phonenumber: '6666666666',
     holes: holes,
   };
-  const path = `admin/${_adminId}/${_tournamentId}/000`;
+  const path = `admin/${_adminId}/${_tournamentId}/000/`;
   const database = firebase.database();
-  database.ref(path).set(data);
+  await database.ref(path).set(data);
 };
 
-export default createTournament;
+const addUser = async (_adminId, _tournamentId, userInfo) => {
+  let max = 0;
+  const { name, phonenumber } = userInfo;
+  const database = firebase.database();
+  const holePath = `admin/${_adminId}/${_tournamentId}/000/holes`;
+  const userPath = `admin/${_adminId}/${_tournamentId}/`;
+  const holes = await database.ref(holePath).once('value');
+  const users = await database.ref(userPath).once('value');
+  Object.keys(users).forEach((userId) => {
+    if (userId.length === 3 && Number(userId) > max) {
+      max = Number(userId);
+    }
+  });
+  users[max + 1] = { name: name, phonenumber: phonenumber, holes: holes };
+  await database.ref(userPath).set(users);
+};
+
+const deleteUser = async (_adminId, _tournamentId, userId) => {
+  const path = `admin/${_adminId}/${_tournamentId}/`;
+  const database = firebase.database();
+  const data = await database.ref(path).once('value');
+  delete data[userId];
+  await database.ref(path).set(data);
+};
+
+export default { createTournament, addUser, deleteUser };
